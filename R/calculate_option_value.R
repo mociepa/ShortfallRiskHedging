9a3b0973d78b06c600bb227899774c0b05f7a0bb
mociepa <michal.ociepa97@gmail.com>
2022-01-26 21:37:41 +0100

    Fix the issue

--- R/calculate_option_value.R
+++ R/calculate_option_value.R
@@ -1,24 +1,24 @@
 #' @title Calculate option values
-#' 
-#' @description 
+#'
+#' @description
 #' The calculate_option_value function calculate option value using Monte Carlo simulations.
-#' 
+#'
 #' @usage calculate_option_value(payoffs, rate, time = 0, End_Time = 1)
-#' 
+#'
 #' @param payoffs numeric vector, payoffs of the option from simulations.
 #' @param rate numeric value, risk free rate in the model rate >= 0.
 #' @param time numeric value, actual time.
 #' @param End_Time numeric value, end time of the option, time < End_Time.
 #' @return A numeric value, estimated value of the option.
-#' 
-#' @examples 
-#' x <- generate_scenarios(100, 0, 0.3)
+#'
+#' @examples
+#' X <- generate_scenarios(100, 0, 0.3)
 #' payoff <- calculate_payoffs(X, call_payoff, strike = 100)
 #' calculate_option_value(payoff, 0)
-#' 
+#'
 #' payoff <- calculate_payoffs(X, put_payoff, strike = 100)
 #' calculate_option_value(payoff, 0)
-#' 
+#'
 #' ## Example with asian call payoff
 #' asian_call <- function(asset_price, strike){
 #'   return(mean(asset_price) - min( mean(asset_price), strike ))
